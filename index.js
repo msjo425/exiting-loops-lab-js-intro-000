c24c8ae9c23fccff737ee2df78dca80b379a1fa9
matt joseloff <msjo425@gmail.com>
2020-05-13 02:11:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function breakOut (array, changeValue, stopValue){
 function keepGoing(array, changeValue, skipValue){
   for (let i=0; i<array.length; i++){
     if (array[i] !== skipValue){
-      array[i] = 
+      array[i] = changeValue
     }
   }
 
